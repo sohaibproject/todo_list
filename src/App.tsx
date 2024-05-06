cca2210a6d83a25ec5ca385143c309df21bae4f0
boukraa sohaib <73255434+sohaibproject@users.noreply.github.com>
2024-05-06 11:56:48 +0100

    add notfound page

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,17 @@
 import './App.css';
-import { HashRouter, Navigate, Route, Routes } from 'react-router-dom';
+import { BrowserRouter, Navigate, Route, Routes } from 'react-router-dom';
 import { Suspense, lazy } from 'react';
 import PrivateRoute from './@core/routes/PrivateRoute';
-// import TodoPage from './views/pages/todo';
-// import LoginPage from './views/pages/login';
+
 const TodoPage = lazy(() => import('./views/pages/todo'));
 const LoginPage = lazy(() => import('./views/pages/login'));
+const NotFound = lazy(() => import('./views/pages/not-found'));
 import { ThemeProvider } from './@core/contexts/themContext';
 import GlobalLoading from './views/components/loading/GlobalLoading';
 
 function App() {
   return (
-    <HashRouter>
+    <BrowserRouter>
       <Suspense fallback={<GlobalLoading />}>
         <Routes>
           <Route path='/' element={<Navigate replace to='/login' />} />
@@ -24,13 +24,11 @@ function App() {
             }
           />
           <Route path='/todos' element={<PrivateRoute element={<TodoPage />} />} />
-          <Route path='/authorazation' element={<>...authorazation </>} />
-
-          <Route path='/404' element={<>...authorazation </>} />
-          <Route path='*' element={<>...authorazation </>} />
+          <Route path='/404' element={<NotFound />} />
+          <Route path='*' element={<NotFound />} />
         </Routes>
       </Suspense>
-    </HashRouter>
+    </BrowserRouter>
   );
 }
 
