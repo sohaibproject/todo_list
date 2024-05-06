fa21bdca535a2c046de1c8dfe19aef0af0fdbbf0
boukraa sohaib <73255434+sohaibproject@users.noreply.github.com>
2024-05-06 12:09:22 +0100

    persist them config in localStorage

--- src/views/layout/navBar/components/ThemSwitcher.tsx
+++ src/views/layout/navBar/components/ThemSwitcher.tsx
@@ -2,18 +2,21 @@ import { useEffect, useState } from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faSun, faMoon } from '@fortawesome/free-solid-svg-icons';
 const ThemSwitcher = () => {
-  const [theme, setTheme] = useState('light');
+  const [theme, setTheme] = useState(localStorage.getItem('theme') ?? 'light');
   useEffect(() => {
     switch (theme) {
       case 'dark':
         document.documentElement.classList.add('dark');
+        localStorage.setItem('theme', 'dark');
         break;
       case 'light':
         document.documentElement.classList.remove('dark');
+        localStorage.setItem('theme', 'light');
 
         break;
       default:
         document.documentElement.classList.remove('dark');
+        localStorage.setItem('theme', 'light');
 
         break;
     }
